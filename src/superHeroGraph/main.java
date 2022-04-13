fb77d5be5e37eae069849b0ce21af49d756cdbd3
Mathis Tanghe <mathistanghe@hotmail.fr>
2022-04-13 17:26:04 +0200

    scrap json

--- src/superHeroGraph/main.java
+++ src/superHeroGraph/main.java
@@ -1,18 +1,63 @@
 package superHeroGraph;
 
-import attributs.Attributs;
-import attributs.Hero;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
-		Attributs attr = new Hero(1);
-		Attributs attr2 = new Hero(2);
-		System.out.println(attr.getAttribut());
-		System.out.println(attr2.getAttribut());
 		
-		attr.addAttributToListAttributs(attr2);
 		
+		// RECUPERATION DES HEROS
+		
+		JSONParser jsonParser = new JSONParser();
+        try (FileReader reader = new FileReader("superHeroes.json"))
+        {
+            //Read JSON file
+            Object obj = jsonParser.parse(reader);
+ 
+            // Max Heroes : 563
+            JSONArray heroesList = (JSONArray) obj;
+            for(int i = 0; i < 10 ; i++)
+            {
+            	//VOIR VOCABULAIRE W3C
+                JSONObject hero = (JSONObject)heroesList.get(i);
+                
+                System.out.println(hero.get("name"));
+                
+                JSONObject heroStats = (JSONObject)hero.get("powerstats");
+                System.out.println(heroStats.get("intelligence"));
+                System.out.println(heroStats.get("strength"));
+                System.out.println(heroStats.get("speed"));
+                System.out.println(heroStats.get("durability"));
+                System.out.println(heroStats.get("power"));
+                System.out.println(heroStats.get("combat"));
+
+                JSONObject heroAppearance = (JSONObject)hero.get("appearance");
+                System.out.println(heroAppearance.get("gender"));
+                System.out.println(heroAppearance.get("race"));
+                
+                JSONObject heroBiography = (JSONObject)hero.get("biography");
+                System.out.println(heroBiography.get("publisher"));
+                System.out.println(heroBiography.get("alignment"));
+                System.out.println(heroBiography.get("alterEgos"));
+                
+                JSONObject heroConnections = (JSONObject)hero.get("connections");
+                System.out.println(heroConnections.get("groupAffiliation"));
+            } 
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
 	}
+
 }
